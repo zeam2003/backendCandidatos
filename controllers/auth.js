3fe863296ad6e86391ad10f27390149b37ac8d23
zeam2003 <zeam2003@outlook.com>
2020-08-10 16:33:09 -0300

    seccion 12 google

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,8 @@ const bcrypt = require('bcryptjs');
 
 const Usuario = require('../models/usuario');
 const { generarJWT } = require('../helpers/jwt');
+const { googleVerify } = require('../helpers/google-verify');
+const usuario = require('../models/usuario');
 
 const login = async(req, res = response) => {
 
@@ -46,6 +48,54 @@ const login = async(req, res = response) => {
     }
 };
 
+const googleSignIn = async(req, res = response) => {
+
+    const googleToken = req.body.token;
+
+    try {
+        const { name, email, picture } = await googleVerify(googleToken);
+
+        // 
+        const usuarioDB = await Usuario.findOne({ email });
+        let usuario;
+
+        if (!usuarioDB) {
+            usuario = new Usuario({
+                nombre: name,
+                email,
+                password: '@@@',
+                img: picture,
+                google: true
+            });
+        } else {
+            // existe usuario
+            usuario = usuarioDB;
+            usuario.google = true;
+            // usuario.password = '@@@'
+        }
+
+        // Guardar en Base de datos
+        await usuario.save();
+
+        // Generar JWT
+        const token = await generarJWT(usuarioDB.id);
+
+        res.json({
+            ok: true,
+            token
+        });
+
+    } catch (error) {
+        res.status(401).json({
+            ok: false,
+            msg: 'Token no es correcto -Google Sign'
+        });
+    }
+
+
+};
+
 module.exports = {
-    login
+    login,
+    googleSignIn
 };
--- index.js
+++ index.js
@@ -17,6 +17,8 @@ app.use(express.json());
 // Base de datos
 dbConnection();
 
+// Directorio Publico
+app.use(express.static('public'));
 
 // Rutas
 app.use('/api/usuarios', require('./routes/usuarios'));
